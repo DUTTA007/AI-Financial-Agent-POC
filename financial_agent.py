bd8fc0d0d4bd8876bcc9e7f41d09dcf086abb20f
Tanmay Dutt <tanmaydutt97@gmail.com>
2025-07-26 22:45:41 +0200

    Agent defination
    Define agents and run first call.

--- financial_agent.py
+++ financial_agent.py
@@ -1,46 +1,38 @@
-from phi.agent import Agent
-from phi.model.groq import Groq
-from phi.tools.yfinance import YFinanceTools
+from phi.agent import agent
+from phi.model.groq import groq
+from phi.tools.yfinance import YFinanceTools 
 from phi.tools.duckduckgo import DuckDuckGo
-import openai
 
-import os
-from dotenv import load_dotenv
-load_dotenv()
-openai.api_key=os.getenv("OPENAI_API_KEY")
 
-## web search agent
-web_search_agent=Agent(
-    name="Web Search Agent",
-    role="Search the web for the information",
-    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-    tools=[DuckDuckGo()],
-    instructions=["Alway include sources"],
-    show_tools_calls=True,
-    markdown=True,
+# Websearch Agent
 
+websearch_agent = agent(
+    name = "Web search agent",
+    role = "Search the web for the information",
+    model = groq(id="compound-beta-kimi"),
+    tools = [DuckDuckGo()],
+    instruction = ["Always include sources"],
+    show_tool_calls = True,
+    markdown = True
 )
 
-## Financial agent
-finance_agent=Agent(
-    name="Finance AI Agent",
-    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-    tools=[
-        YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,
-                      company_news=True),
-    ],
-    instructions=["Use tables to display the data"],
-    show_tool_calls=True,
-    markdown=True,
+# Financial Agent 
 
+finance_agent = agent(
+    name = "Fiancial Agent",
+    model = groq(id="compound-beta-kimi"),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,company_news=True, historical_prices= True)],
+    instruction = ["Use Tables to display Data"],
+    show_tool_calls = True,
+    markdown = True
 )
 
-multi_ai_agent=Agent(
-    team=[web_search_agent,finance_agent],
-    instructions=["Always include sources","Use table to display the data"],
-    show_tool_calls=True,
-    markdown=True,
+
+multi_agent = Agent(
+    team = (websearch_agent, finance_agent),
+    instructions = ["Always include sources","Use table to display the data"],
+    show_tool_calls = True,
+    markdown = True
 )
 
-multi_ai_agent.print_response("Summarize analyst recommendation and share the latest news for NVDA",stream=True)
-
+multi_agent.print_response("Summarize analyst recommendation and share the latest news for NV")
