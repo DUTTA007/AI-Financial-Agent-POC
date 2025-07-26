4f79132ea82b99bab19cd8c953c0eaa8b8f36e2f
Tanmay Dutt <tanmaydutt97@gmail.com>
2025-07-27 00:14:59 +0200

    Fix OpenAI error and use llma model instead

--- financial_agent.py
+++ financial_agent.py
@@ -1,36 +1,45 @@
-from phi.agent import agent
-from phi.model.groq import groq
+from phi.agent import Agent
+from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools 
 from phi.tools.duckduckgo import DuckDuckGo
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+# # For Open API
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()  # Loads from .env file
+# api_key = os.getenv("OPENAI_API_KEY")
 
 
 # Websearch Agent
 
-websearch_agent = agent(
+websearch_agent = Agent(
     name = "Web search agent",
     role = "Search the web for the information",
-    model = groq(id="compound-beta-kimi"),
+    model = Groq(id="llama-3.3-70b-versatile"),
     tools = [DuckDuckGo()],
-    instruction = ["Always include sources"],
+    instructions = ["Always include sources"],
     show_tool_calls = True,
     markdown = True
 )
 
 # Financial Agent 
 
-finance_agent = agent(
+finance_agent = Agent(
     name = "Fiancial Agent",
-    model = groq(id="compound-beta-kimi"),
+    model = Groq(id="llama-3.3-70b-versatile"),
     tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,company_news=True, historical_prices= True)],
-    instruction = ["Use Tables to display Data"],
+    instructions = ["Use Tables to display Data"],
     show_tool_calls = True,
     markdown = True
 )
 
-
 multi_agent = Agent(
     team = (websearch_agent, finance_agent),
     instructions = ["Always include sources","Use table to display the data"],
+    model = Groq(id="llama-3.3-70b-versatile"),
     show_tool_calls = True,
     markdown = True
 )
